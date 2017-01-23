0a153017b8cf79fc376f164353a2e5cb807c6ec9
D1V3 <d1v3@outlook.com>
2017-01-23 20:13:42 +0000

    Added lists of members to queries ... not yet finished.

--- Server/Membership_Report.py
+++ Server/Membership_Report.py
@@ -11,14 +11,14 @@ import re
 EPOCH = datetime.datetime.utcfromtimestamp(0)
 
 def unix_time_millis(dt):
-	return (dt - EPOCH).total_seconds()
+	return int(round((dt - EPOCH).total_seconds()*1000))
 
 
 #===============================   MAIN   ====================================
 
 #Specify the connection to the mongo database:
-#client = MongoClient('mongodb://localhost:27017')
-client = MongoClient('mongodb://192.168.1.3:27017')
+client = MongoClient('mongodb://localhost:27017')
+#client = MongoClient('mongodb://192.168.1.3:27017')
 
 db = client['makerauth']
 members = db['members']
@@ -28,56 +28,69 @@ reportFile = open('membership_report.txt','w')
 reportFile.write("=========	MEMBERSHIP REPORT	=========\n")
 
 today = datetime.datetime.now()
-todayPretty = today.strftime("%m%d%Y")
+todayPretty = today.strftime("%b-%d-%Y")
 todayShort = today.strftime("%b-%d")
 todayEpoch = unix_time_millis(today)
 
-#fromDate = today - timedelta(days=14)
-fromDate = today - timedelta(days=600)
-fromDatePretty = fromDate.strftime("%m%d%Y")
+fromDate = today - timedelta(days=14)
+fromDatePretty = fromDate.strftime("%b-%d-%Y")
 fromDateShort = fromDate.strftime("%b-%d")
 fromDateEpoch = unix_time_millis(fromDate)
 
-reportFile.write("Date Range:	%s - %s\n\n" % (fromDatePretty,todayPretty))
+reportFile.write("Date Range:	%s -to- %s\n\n" % (fromDatePretty,todayPretty))
 
-#Run queries on the 'members' collection, and format the results.
-reportFile.write("fromDate == %s\n"%(fromDate))
-reportFile.write("fromDateEpoch == %s\n"%(fromDateEpoch))
-reportFile.write("EPOCH == %s\n"%(EPOCH))
-
+#----------------------------------------- Run queries on the 'members' collection, and format the results.
 #Total members: (all member records in db)
 numTotal = members.count()
 reportFile.write("\nTotal Member Records == %s\n"%(numTotal))
 
 #Members gained: (new members in the past 2 weeks)
+reportFile.write("\n=========== GAINED =============\n")
 numGained = members.count({"startDate":{"$gte":fromDateEpoch}})
 reportFile.write("Members Gained since (%s) == %s\n" % (fromDateShort,numGained))
 regex_fullname=re.compile(".*fullname.*:\su'([\w\s]*)")
+whoGained = members.find({"startDate":{"$gte":fromDateEpoch}},{"_id":0,"fullname":1})
 fullName = "NO_NAME"
-for member in members.find({"startDate":{"$gte":fromDateEpoch}},{"_id":0,"fullname":1}):
+for member in whoGained:
 	#Who are they?
 	fullName = regex_fullname.search(str(member)).group(1)
-	reportFile.write("	%s\n" % (fullName))
+	reportFile.write("%s\n" % (fullName))
 
 #Members lost: (members expired in the past 2 weeks)
-numLost = members.count({"expirationTime":{"$gte":fromDateEpoch}})
-reportFile.write("Members Lost since (%s) == %s\n" % (todayShort,numLost))
+reportFile.write("\n=========== LOST =============\n")
+numLost = members.count({"expirationTime":{"$gte":fromDateEpoch,"$lte":todayEpoch}})
+whoLost = members.find({"expirationTime":{"$gte":fromDateEpoch,"$lte":todayEpoch}},{"_id":0,"fullname":1})
+reportFile.write("Members Lost since (%s) == %s\n" % (fromDateShort,numLost))
 fullName = "NO_Name" #Keep this here so that we don't accidentally carry over values
-for member in members.find({"expirationTime":{"$gte":fromDateEpoch}},{"_id":0,"fullname":1}):
+for member in whoLost:
 	#Who are they?
 	fullName = regex_fullname.search(str(member)).group(1)
-	reportFile.write("	%s\n" % (fullName))
+	reportFile.write("%s\n" % (fullName))
 
 #Members in good standing:
+reportFile.write("\n=========== IN GOOD STANDING =============\n")
 numGoodStanding = members.count({"expirationTime":{"$gte":todayEpoch}})
-reportFile.write("Members in Good Standing == %s\n" % (numGoodStanding))
+reportFile.write("Individual Members in Good Standing == %s\n" % (numGoodStanding))
+indvMem = members.find({"expirationTime":{"$gte":todayEpoch}},{"_id":0,"fullname":1})
+fullName = "NO_Name"
+for member in indvMem:
+	#Who are they?
+	fullName = regex_fullname.search(str(member))
+	reportFile.write("%s\n" % (fullName))
+
 
 #NOTE: find() returns a Cursor instance, which allows us to iterate over all matching documents.
 #Find all group members.
 numGM = members.count({"groupName":{"$exists":True , "$ne":""}})
 reportFile.write("Group Members in Good Standing == %s\n" % (numGM))
-#for member in members.find({"groupName":{"$exists":True , "$ne":""}}):
-#	reportFile.write(str(pprint.pprint(member)))
+#groupMem = members.find({"expirationTime":{"$gte":todayEpoch,"groupName":{"$exists":True}}},{"_id":0,"fullname":1,"groupName":1})
+groupMem = members.find({"groupName":{"$exists":True}},{"_id":0,"fullname":1})
+fullName = "NO_Name"
+for member in groupMem:
+	#Sort by group, who are they?
+	fullName = regex_fullname.search(str(member))
+	reportFile.write("%s - group = \n" % (fullName))
+
 
 
 #How many members have signed a contract?
